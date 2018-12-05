30713b305127e52883dcec24077450acfa7d7388
guoxiaodi <guody001@gmail.com>
2018-12-06 02:26:27 +0800

    code

--- model/admin/adminCategory.js
+++ model/admin/adminCategory.js
@@ -6,8 +6,7 @@ var AdminCategory = db.defineModel('adminCategory',{
     categoryName:seq.STRING(50),
     menuId:seq.INTEGER,
     routeName:seq.STRING(100),
-    sortNo:seq.INTEGER,
-    routeName:seq.STRING(100)
+    sortNo:seq.INTEGER
 })
 AdminCategory.sync();
 module.exports = AdminCategory;
